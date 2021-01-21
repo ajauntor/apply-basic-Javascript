3ea78bfa65374c4fbc81bc971952f90ae8937b3a
AJ Auntor <auntorknocks@gmail.com>
2021-01-21 17:17:01 +0600

    apply Conslet and Factorial

---
+++ constLet.js
@@ -0,0 +1,12 @@
+// var flowers = "Rose"
+// if(flowers.length > 5){
+//     flowers = "Belly"
+// }
+// console.log(flowers);
+
+// const myName = "AJ Auntor"
+// console.log(myName);
+let myName = "AJ Auntor"
+myName = "Hasan Raza"
+myName = 15
+console.log(myName);
---
+++ factorial.js
@@ -0,0 +1,11 @@
+function fact (num){
+    var fact = 1;
+    for( var i =1; i <= num; i++){
+        fact = fact * i;
+    }
+    return fact;
+}
+var result = fact(5);
+console.log(result);
+var final = fact(8);
+console.log(final);
