74ffc55f8c76661b912b495488cdda988721d78b
AJ Auntor <auntorknocks@gmail.com>
2021-01-21 17:19:33 +0600

    More about factorial

---
+++ factorialWhile.js
@@ -0,0 +1,11 @@
+function facotor(n) {
+    var ten = 8;
+    var facotor = 1;
+    while (ten <= n) {
+        facotor = facotor * ten;
+        ten++;
+    }
+    return facotor;
+}
+var result = facotor(12);
+console.log(result);
