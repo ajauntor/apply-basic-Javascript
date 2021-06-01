54bd6d16b9d8d2762daac2287de98cd78bec8740
AJ Auntor <auntorknocks@gmail.com>
2021-06-01 11:56:18 +0600

    check prime

--- checkPrime.js
+++ checkPrime.js
@@ -2,7 +2,7 @@ var n = 28;
 for(i = 2; i < n-1; i++){
     console.log(i, n % i);
     if(n % i == 0){
-        console.log("not a prime number");
+        // console.log("not a prime number");
         break;
     }
 }
