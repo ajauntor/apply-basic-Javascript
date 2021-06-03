6384a0f99c3bd8a3037a6778e018b7d7ef2df458
AJ Auntor <auntorknocks@gmail.com>
2021-06-03 12:03:33 +0600

    factorial

--- factorial.js
+++ factorial.js
@@ -1,15 +1,3 @@
-// function fact (num){
-//     var fact = 1;
-//     for( var i =1; i <= num; i++){
-//         fact = fact * i;
-//     }
-//     return fact;
-// }
-// var result = fact(5);
-// console.log(result);
-// var final = fact(8);
-// console.log(final);
-
 function fact (num){
     var fact = 1;
     for( var i =1; i <= num; i++){
