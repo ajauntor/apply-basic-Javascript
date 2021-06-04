e1a300586851e9de80ed90d212750040e433561f
AJ Auntor <auntorknocks@gmail.com>
2021-06-04 12:09:10 +0600

    factorial While

--- factorialRecursive.js
+++ factorialRecursive.js
@@ -1,2 +1,13 @@
 // Factorial while........
 
+function facotor(n) {
+    var ten = 8;
+    var facotor = 1;
+    while (ten <= n) {
+        facotor = facotor * ten;
+        ten++;
+    }
+    return facotor;
+}
+// var result = facotor(12);
+// console.log(result);
