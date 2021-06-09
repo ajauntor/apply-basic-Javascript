70cfe45f721d33f207a8dce225bd09a7bc19d348
AJ Auntor <auntorknocks@gmail.com>
2021-06-09 12:18:40 +0600

    inchFeet

--- inch-to-feet.js
+++ inch-to-feet.js
@@ -6,9 +6,3 @@ var day1 = inchToFeet(144);
 console.log(day1);
 var da2 = inchToFeet(300);
 console.log(da2);
-
-
-// var day1 = inchToFeet(144);
-// console.log(day1);
-// var da2 = inchToFeet(300);
-// console.log(da2);
