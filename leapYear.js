44b9f5ce0f438d9443e7d15bcfe3d5083f91d9a4
AJ Auntor <auntorknocks@gmail.com>
2021-06-29 12:31:16 +0600

    leapYear Test

--- leapYear.js
+++ leapYear.js
@@ -9,12 +9,12 @@ function isLeapYear(leapYear) {
 }
 
 
-// function isLeapYear(leapYear) {
-//     const remainder = leapYear % 4;
-    if (remainder == 0) {
-        return true;
-    }
-    else {
-        return false;
-    }
-}
+// // function isLeapYear(leapYear) {
+// //     const remainder = leapYear % 4;
+//     if (remainder == 0) {
+//         return true;
+//     }
+//     else {
+//         return false;
+//     }
+// }
